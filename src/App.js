da6f08ea052c1bd00630c007874e3302e26825ba
MeerHamza1421 <hamzababar37@gmail.com>
2020-08-03 12:54:51 +0500

    fixing previous issues

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Component/Navbar';
+import InputComponent from './Component/InputComponent';
+import './App.css'
+const App = () => (
+  <>
+    <div>
+      <Navbar />
+      <InputComponent />
+    </div>
+
+  </>
+);
+
+
+export default App;
